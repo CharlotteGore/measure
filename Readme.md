aa7f50c3d56c771dbadf89998de21b385ab74cbe
Charlotte Gore <conspiracygore@gmail.com>
2013-02-26 12:41:47 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,56 @@
+
+# measure
+
+  A module to measure elements. Cross browser. 
+
+## Installation
+
+    $ component install charlottegore/measure
+
+## API
+
+    var measurer = require('measure')( element );
+
+### .pagePosition()
+
+  The position of the element in the page
+
+    var position = measurer.pagePosition();
+
+    Returns an object with properties `x` and `y`
+
+### .innerPosition()
+
+  The position of the element relative to its offset parent.
+
+    var position = measurer.innerPosition();
+
+    Returns an object with properties `x` and `y`
+
+### .boxDetails()
+
+  The position of the element relative to its offset parent.
+
+    var position = measurer.innerPosition();
+
+    Returns an object with properties `border` and `margin` and `padding`. Each contains an object with the properties, `top`, `left`, `bottom` and `right`
+
+### .innerSize()
+ 
+  The size of the object, not including borders, margin or padding.
+
+    var position = measurer.innerSize();
+
+    Returns an object with properties `x` and `y` 
+
+### .outerSize()
+ 
+  The size of the object, including borders, margin or padding.
+
+    var position = measurer.outerSize();
+
+    Returns an object with properties `x` and `y` 
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,144 @@
+var Measure = function(el){
+
+  this.el = el;
+
+  return this;
+
+}
+
+var IEStyles = {
+  "padding-top" : "paddingTop",
+  "padding-bottom" : "paddingBottom",
+  "padding-left" : "paddingLeft",
+  "padding-right" : "paddingRight",
+  "border-top-width" : "borderTopWidth",
+  "border-left-width" : "borderLeftWidth",
+  "border-right-width" : "borderRightWidth",
+  "border-bottom-width" : "borderBottomWidth",
+  "margin-top" : "marginTop",
+  "margin-left" : "marginLeft",
+  "margin-right" : "marginRight",
+  "margin-bottom" : "marginBottom"
+};
+
+var getStyle = function(property, e){
+
+  var val;
+  if(e.currentStyle){
+    
+    val = parseInt( ( e.currentStyle[ IEStyles[ property ] ] ).replace( 'px', '' ), 10 );
+  
+  }else if(window.getComputedStyle){
+
+    val = parseInt( (document.defaultView.getComputedStyle( e, null ).getPropertyValue( property ) ).replace('px', '') ,10 );
+
+  }
+
+  return val;
+
+};
+
+var getBoxDetail = function(e){
+
+  var padding={}, border={}, margin={};
+
+  padding.top = getStyle("padding-top", e);
+  padding.right = getStyle("padding-right", e);
+  padding.bottom = getStyle("padding-bottom", e);
+  padding.left = getStyle("padding-left", e);
+
+  margin.top = getStyle("margin-top", e);
+  margin.right = getStyle("margin-right", e);
+  margin.bottom = getStyle("margin-bottom", e);
+  margin.left = getStyle("margin-left", e);
+
+  border.top = getStyle("border-top-width", e);
+  border.right = getStyle("border-right-width", e);
+  border.bottom = getStyle("border-bottom-width", e);
+  border.left = getStyle("border-left-width", e);
+
+  return{
+    padding: padding,
+    border: border,
+    margin: margin
+  }
+
+};
+
+Measure.prototype = {
+
+  pagePosition : function(){
+
+    var curleft = 0, curtop = 0, e = this.el;
+
+    if (e.offsetParent) {
+      do {
+        curleft += e.offsetLeft;
+        curtop += e.offsetTop;
+      } while (e = e.offsetParent);
+
+      return {x : curleft, y : curtop};
+    }
+
+  },
+
+  innerPosition : function(){
+
+    var m = boxDetails.margin;
+
+    var xOffset = m.left;
+    var yOffset = m.top;  
+
+    var curleft = 0, curtop = 0;
+
+    curleft += (el.offsetLeft - xOffset);
+    curtop += (el.offsetTop - yOffset);
+
+    return {x : curleft, y : curtop};
+
+  },
+
+  boxDetails : function(){
+
+    return getBoxDetail( this.el );
+
+  },
+
+  innerSize : function(){
+
+    var e = this.el;
+
+    var boxDetails = getBoxDetail(e);
+
+    var p = boxDetails.padding;
+    var b = boxDetails.border;
+
+    var xOffset = p.left + p.right + b.left + b.right;
+    var yOffset = p.top + p.bottom + b.top + b.bottom;
+
+    var x = 0, y = 0;
+    x = (Math.max(el.clientWidth, el.offsetWidth)) - xOffset;
+    y = (Math.max(el.clientHeight, el.offsetHeight)) - yOffset;
+    return {x : x, y : y};
+
+  },
+
+  outerSize : function(){
+
+    var x = 0, y = 0, e = this.el;
+
+    x = Math.max(e.clientWidth, e.offsetWidth);
+    y = Math.max(e.clientHeight, e.offsetHeight);
+
+    return {x : x, y : y};
+
+  }
+
+
+};
+
+module.exports = function(el){
+
+  return new Measure(el);
+
+};

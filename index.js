ab0ccdd76eceb347422d1ffde73f78df4ff99396
Charlotte Gore <conspiracygore@gmail.com>
2013-11-09 03:28:05 +0000

    Update index.js
    Somehow something was using this but it was never in the repo. Bizarre.

--- index.js
+++ index.js
@@ -137,7 +137,19 @@ Measure.prototype = {
 
     return {x : x, y : y};
 
-  }
+  },
+  screenSize : function(){
+ 
+    var w = window,
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        x = w.innerWidth || e.clientWidth || g.clientWidth,
+        y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+ 
+    return { x : x, y : y}
+ 
+  } 
 
 
 };
